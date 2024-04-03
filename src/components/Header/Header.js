3bcc879604798281534278ad8a9fa3b0d6ae05df
Yukiuyeda <155521329+Yukiuyeda@users.noreply.github.com>
2024-04-04 00:16:36 +0900

    test commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+        <a href="#">Blog</a>
+        <a href="#">お問い合わせ</a>
+    </header>
+  )
+}
+
+export default Header
