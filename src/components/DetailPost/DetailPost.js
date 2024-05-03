727f8cf5b515fbbf45e9273dd2170a9d5954c8e3
yukiuyeda <yueda940408@gmail.com>
2024-05-04 00:20:37 +0900

    課題3の実装

--- src/components/DetailPost/DetailPost.js
+++ src/components/DetailPost/DetailPost.js
@@ -1,37 +1,64 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import { posts } from '../../data/posts';
-import styles from "./DetailPost.module.css"
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+// import { posts } from '../../data/posts';
+import styles from "./DetailPost.module.css";
 
 const DetailPost = () => {
-    const { id } = useParams();
-    const post = posts.find(post => post.id === Number(id));
-    // console.log(post);
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    //記事が見つからなかったとき
-    if (!post) {
-        return <div>記事が見つかりません</div>
+  // APIでpostsを取得する処理をuseEffectで実行します。
+  useEffect(() => {
+    const fetcher = async () => {
+      const res = await fetch(
+        "https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts"
+      );
+      const data = await res.json();
+      setPosts(data.posts);
+      setIsLoading(false);
     };
 
-    return (
-        <div>
-            <div className={styles.container}>
-                <img src={post.thumbnailUrl} />
-                <div className={styles.article}>
-                    <div className={styles.dateAndCategories}>
-                        <p className={styles.date}>{new Date(post.createdAt).toLocaleDateString()}</p>
-                        <ul className={styles.categories}>
-                            {post.categories.map(category => {return <li key={category}>{category}</li>})}
-                        </ul>
-                    </div>
-                    <h3 className={styles.title}>{`APIで取得した${post.title}`}</h3>
-                    <div className='detailContent' dangerouslySetInnerHTML={{__html: post.content}}></div>
-                </div>
-            </div>
-                
-        </div>
-            
-    )
-}
+    fetcher();
+  }, []);
 
-export default DetailPost
+  const { id } = useParams();
+  console.log(id);
+  const post = posts.find((post) => post.id === Number(id));
+  // console.log(post);
+
+  //記事が見つからなかったとき
+  if (!isLoading && !post) {
+    return <div>記事が見つかりません</div>;
+  }
+
+  return (
+    <div>
+      {isLoading ? (
+        <div>読み込み中…</div>
+      ) : (
+        <div className={styles.container}>
+          <img src={post.thumbnailUrl} />
+          <div className={styles.article}>
+            <div className={styles.dateAndCategories}>
+              <p className={styles.date}>
+                {new Date(post.createdAt).toLocaleDateString()}
+              </p>
+              <ul className={styles.categories}>
+                {post.categories.map((category) => {
+                  return <li key={category}>{category}</li>;
+                })}
+              </ul>
+            </div>
+            <h3 className={styles.title}>{`APIで取得した${post.title}`}</h3>
+            <div
+              className="detailContent"
+              dangerouslySetInnerHTML={{ __html: post.content }}
+            ></div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default DetailPost;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,58 +1,75 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 // import { posts } from '../../data/posts';
-import styles from "./Main.module.css"
-import { Link } from 'react-router-dom';
+import styles from "./Main.module.css";
+import { Link } from "react-router-dom";
 
 const Main = () => {
-
-  const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   // APIでpostsを取得する処理をuseEffectで実行します。
   useEffect(() => {
     const fetcher = async () => {
-      const res = await fetch("https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts")
-      const data = await res.json()
-      console.log(data);
-      setPosts(data.posts)
-    }
+      const res = await fetch(
+        "https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts"
+      );
+      const data = await res.json();
+      // console.log(data);
+      setPosts(data.posts);
+      setIsLoading(false);
+    };
 
-    fetcher()
-  }, [])
-  
+    fetcher();
+  }, []);
+
   return (
     <div>
-      {!posts ? (
-      <div>読み込み中…</div>
-    ):(
-      <div className={styles.mainContainer}>
-      <ul className={styles.posts}>
-        {posts.map(post => {
-          return (<li key={post.id} className={styles.mainList}>
-            <Link to={`/posts/${post.id}`} className={styles.mainLink}>
-              <div className={styles.postContainer}>
-                <div className={styles.post}>
-                  <div className={styles.dateAndCategories}>
-                    <p className={styles.date}>{new Date(post.createdAt).toLocaleDateString()}</p>
-                    <ul className={styles.categories}>
-                      {post.categories.map(category => {return <li key={category} className={styles.categoriesList}>{category}</li>})}
-                    </ul>
-                  </div>
-                  <div className={styles.title}>{`APIで取得した${post.title}`}</div>  
-                  <div className={styles.content} dangerouslySetInnerHTML={{__html: post.content}}></div>
-                </div>  
-              </div> 
-            </Link>
-           
-            
-          </li>)
-        })}
-      </ul>
+      {isLoading ? (
+        <div>読み込み中…</div>
+      ) : (
+        <div className={styles.mainContainer}>
+          <ul className={styles.posts}>
+            {posts.map((post) => {
+              return (
+                <li key={post.id} className={styles.mainList}>
+                  <Link to={`/posts/${post.id}`} className={styles.mainLink}>
+                    <div className={styles.postContainer}>
+                      <div className={styles.post}>
+                        <div className={styles.dateAndCategories}>
+                          <p className={styles.date}>
+                            {new Date(post.createdAt).toLocaleDateString()}
+                          </p>
+                          <ul className={styles.categories}>
+                            {post.categories.map((category) => {
+                              return (
+                                <li
+                                  key={category}
+                                  className={styles.categoriesList}
+                                >
+                                  {category}
+                                </li>
+                              );
+                            })}
+                          </ul>
+                        </div>
+                        <div
+                          className={styles.title}
+                        >{`APIで取得した${post.title}`}</div>
+                        <div
+                          className={styles.content}
+                          dangerouslySetInnerHTML={{ __html: post.content }}
+                        ></div>
+                      </div>
+                    </div>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
     </div>
-    )}
-    </div>
-    
-      
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
